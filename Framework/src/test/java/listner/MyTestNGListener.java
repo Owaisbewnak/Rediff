62e98e4b8dbe28dbeaa6afec1479084e49e03890
owais <owaisbewnak1416@gmail.com>
2026-09-22 14:15:24 +0530

    Fix NullPointerException in TestNG Listener

--- Framework/src/test/java/listner/MyTestNGListener.java
+++ Framework/src/test/java/listner/MyTestNGListener.java
@@ -2,23 +2,37 @@ package listner;
 
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-
 import com.aventstack.extentreports.ExtentTest;
 
 public class MyTestNGListener implements ITestListener {
 
-	public void onTestFailure(ITestResult result) {
-		ExtentTest test = (ExtentTest) result.getTestContext().getAttribute("extentTest");
-		test.fail(result.getThrowable().getMessage());
-	}
-	
-	public void onTestSuccess(ITestResult result) {
-		ExtentTest test = (ExtentTest) result.getTestContext().getAttribute("extentTest");
-		test.pass("Test Success : " + result.getName());
-	}
-	
-	public void onTestSkipped(ITestResult result) {
-		ExtentTest test = (ExtentTest) result.getTestContext().getAttribute("extentTest");
-		test.skip(result.getName() + " :: Test Skipped Due to Critical Error in Previous Test");
-	}
+    @Override
+    public void onTestFailure(ITestResult result) {
+        ExtentTest test = (ExtentTest) result.getTestContext().getAttribute("extentTest");
+        if (test != null) {
+            test.fail(result.getThrowable().getMessage());
+        } else {
+            System.out.println("Test Failed (ExtentTest was null): " + result.getName());
+        }
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        ExtentTest test = (ExtentTest) result.getTestContext().getAttribute("extentTest");
+        if (test != null) {
+            test.pass("Test Success : " + result.getName());
+        } else {
+            System.out.println("Test Passed (ExtentTest was null): " + result.getName());
+        }
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        ExtentTest test = (ExtentTest) result.getTestContext().getAttribute("extentTest");
+        if (test != null) {
+            test.skip(result.getName() + " : Test Skipped Due to Critical Error in Previous Test");
+        } else {
+            System.out.println("Test Skipped (ExtentTest was null): " + result.getName());
+        }
+    }
 }
